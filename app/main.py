bb9de561b4ec9ad0b55e5d06a2b887bd01c91c07
Axel Amigo Arnold <axl89@users.noreply.github.com>
2022-06-26 00:06:32 +0200

    WIP: More endpoints

--- app/main.py
+++ app/main.py
@@ -1,10 +1,22 @@
 from fastapi import FastAPI
 from redis import Redis
+from redis.exceptions import ResponseError
+
 from os import getenv
 
 app = FastAPI()
 
 r = Redis(host=getenv('REDIS_HOST'))
+
+@app.get("/stream/ajos")
+async def ajo_stream(count: int = 50, id: int = 0):
+    ajos = r.xread({"ajobus": id}, count)
+    return ajos
+
+@app.get("/player/{player_id}")
+async def get_player(player_id):
+    ajos = r.zscore("lb", player_id)
+    return {"player_id": ajos}
 
 @app.get("/leaderboard")
 async def leaderboard():
@@ -18,8 +30,8 @@ async def leaderboard():
 
     i = 0
     for player_tuple in lb:
-        _,amount = player_tuple
-        result[name_list[i]] = amount
+        id,amount = player_tuple
+        result[id] = {"amount": amount, "name": name_list[i]}
         i += 1
 
     return result
